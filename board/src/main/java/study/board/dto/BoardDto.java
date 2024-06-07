aa0a529f077a64c1c9772469d7bdde50b4c26f01
jihyun <cat2998@naver.com>
2024-06-07 14:25:53 +0900

    feat Dto

--- board/src/main/java/study/board/dto/BoardDto.java
+++ board/src/main/java/study/board/dto/BoardDto.java
@@ -1,4 +1,19 @@
 package study.board.dto;
 
-public class BoardDto {
+import study.board.domain.Board;
+
+public record BoardDto(
+        Long id,
+        Long userId,
+        String title,
+        String content
+) {
+    public BoardDto(Board board) {
+        this(
+                board.getId(),
+                board.getUserId(),
+                board.getTitle(),
+                board.getContent()
+        );
+    }
 }
--- board/src/main/java/study/board/dto/UserDto.java
+++ board/src/main/java/study/board/dto/UserDto.java
@@ -1,4 +1,20 @@
 package study.board.dto;
 
-public class UserDto {
+import study.board.domain.User;
+import study.board.domain.UserRole;
+
+public record UserDto(
+        Long id,
+        String username,
+        String password,
+        UserRole role
+) {
+    public UserDto(User user) {
+        this(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getRole()
+        );
+    }
 }
