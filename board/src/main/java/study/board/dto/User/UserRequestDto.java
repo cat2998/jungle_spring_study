dff45471df3d3ab35818aad3c7c4606a99b1e5b7
jihyun <cat2998@naver.com>
2024-06-11 00:35:29 +0900

    modify userrequestdto valid pattern

--- board/src/main/java/study/board/dto/User/UserRequestDto.java
+++ board/src/main/java/study/board/dto/User/UserRequestDto.java
@@ -1,12 +1,19 @@
 package study.board.dto.User;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import study.board.domain.User;
 
 public record UserRequestDto(
-        @Pattern(regexp = "[a-z0-9]{4,10}", message = "최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9)로 구성되어야 합니다.")
+        @NotNull
+        @Size(min = 4, max = 10)
+        @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9)로 구성되어야 합니다.")
         String username,
-        @Pattern(regexp = "[a-zA-Z0-9@$!%*?&]{8,15}", message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자(@,$,!,%,*,?,&)로 구성되어야 합니다.")
+
+        @NotNull
+        @Size(min = 8, max = 15)
+        @Pattern(regexp = "^[a-zA-Z0-9@$!%*?&]{8,15}$", message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자(@,$,!,%,*,?,&)로 구성되어야 합니다.")
         String password
 ) {
         public User toEntity() {
