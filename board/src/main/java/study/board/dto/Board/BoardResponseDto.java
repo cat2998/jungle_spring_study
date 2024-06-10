b12c282974141f5c63410c32588006128246de0b
jihyun <cat2998@naver.com>
2024-06-10 15:24:40 +0900

    add username to response

--- board/src/main/java/study/board/dto/Board/BoardResponseDto.java
+++ board/src/main/java/study/board/dto/Board/BoardResponseDto.java
@@ -6,11 +6,11 @@ import java.time.LocalDateTime;
 public record BoardResponseDto(
         Long id,
         String title,
+        String username,
         String content,
-        LocalDateTime createdAt,
-        LocalDateTime modifiedAt
+        LocalDateTime createdAt
 ) {
     public static BoardResponseDto of(Board board) {
-        return new BoardResponseDto(board.getId(), board.getTitle(), board.getContent(), board.getCreatedAt(), board.getModifiedAt());
+        return new BoardResponseDto(board.getId(), board.getTitle(), board.getUser().getUsername(), board.getContent(), board.getCreatedAt());
     }
 }
